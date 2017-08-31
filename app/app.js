7db6312d00a44882a8976e243d9b9e4d018c078c
Patrick <life0001@126.com>
2017-08-31 18:17:02 +0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,23 +3,32 @@
  */
 import React, {Component} from 'react';
 import { Line, Circle } from 'rc-progress'; //进度条
+import { Progress, Button } from 'antd-mobile';  //进度条
+
 export default class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            percent: 30
+            percent: 50, // 进度
+            bgcolor: '#ddd' // 底色
         }
     }
    render() {
-       const { percent } = this.state;
+       const { percent, bgcolor} = this.state;
        return (
            <div>
-               <Line percent={percent} //进度
+               <Line percent={percent}
                      strokeWidth={2} //进度条粗细
                      trailWidth={2} //代表总和的线条宽度，永远<=上面的值strokeWidth
                      strokeColor="#f60" //进度条颜色
-                     trailColor="#D3D3D3"
+                     trailColor={bgcolor}
                      width="90%"
+               />
+
+               <Progress
+                   percent={percent}
+                   position="fixed" //进度条的位置，fixed 将浮出固定在最顶层，可选: fixed normal
+                   unfilled="show" //是否隐藏未填充轨道，可选：show hide 默认show
                />
            </div>
        )
